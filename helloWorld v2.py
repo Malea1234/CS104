70fad3ce4bb3211904a6e92aa1aa4c2a4d4edec7
Malea1234 <91150663+Malea1234@users.noreply.github.com>
2021-09-21 14:27:49 -0400

    Update helloWorld v2.py

--- helloWorld v2.py
+++ helloWorld v2.py
@@ -1,6 +1,6 @@
 # This is Version 1 of my hello world program
-print('Hello my name is Malea')
+>>>print('Hello my name is Malea')
 # this is version 2 of my Hello Program
-print ('Hello my name is Malea Horn Attanasio')
-
-
+>>>print ('Hello my name is Malea Horn Attanasio')
+Helo my name is Malea Horn Attansio
+>>>
