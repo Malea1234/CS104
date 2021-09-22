8a07e58f3a2ba689b85cd92e27276213fc5b611f
Malea1234 <91150663+Malea1234@users.noreply.github.com>
2021-09-22 14:19:49 -0400

    Delete helloWorld v2.py

--- helloWorld v2.py
+++
@@ -1,6 +0,0 @@
-# This is Version 1 of my hello world program
->>>print('Hello my name is Malea')
-# this is version 2 of my Hello Program
->>>print ('Hello my name is Malea Horn Attanasio')
-Helo my name is Malea Horn Attansio
->>>
